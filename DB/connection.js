8039e40a0314c7c59dc77eb029a4d17b4504bd3f
Marwan-Elmallah <engmarwan0110@gmail.com>
2023-11-28 16:32:12 +0200

    update DB to online

--- DB/connection.js
+++ DB/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 
 const initConnection = () => {
-    mongoose.connect(process.env.CONNECTION_STRING)
+    mongoose.connect(process.env.CONNECTION_STRING_ONLINE)
         .then((res) => console.log("DB Connected.."))
         .catch((err) => console.log("DB Error to conect", err))
 }
