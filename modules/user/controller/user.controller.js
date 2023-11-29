ddfef6babf71e32002038d1978fc9c499e80b9a8
Marwan-Elmallah <engmarwan0110@gmail.com>
2023-11-29 16:50:05 +0200

    remove passwod and bankAcc and oldId from allUsers

--- modules/user/controller/user.controller.js
+++ modules/user/controller/user.controller.js
@@ -7,7 +7,7 @@ const CryptoJS = require("crypto-js");
 const allUser = async (req, res) => {
     try {
         const { start, qty } = req.query
-        const users = await userModel.find({}).select("-password").skip(start).limit(qty)
+        const users = await userModel.find({}).select(["-password", "-bankAcc", "-oldId"]).skip(start).limit(qty)
         res.json({ message: "Done", users })
     } catch (error) {
         res.status(500).json({ message: "Error To Get All Users", error })
